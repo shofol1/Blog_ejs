e0c5994510befe6c0c3961ca997cda74266ae0ce
shofol <18103156@iubat.edu>
2022-05-19 13:01:44 +0600

    all challenge done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -10,6 +11,8 @@ const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const posts = [];
 
 const app = express();
 
@@ -19,7 +22,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", { homeStartingContent: homeStartingContent });
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    composePost: posts,
+  });
 });
 app.get("/about", (req, res) => {
   res.render("about", { aboutContent: aboutContent });
@@ -27,7 +33,35 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
   res.render("contact", { contactContent: contactContent });
 });
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
 
+app.post("/compose", (req, res) => {
+  const title = req.body.title;
+  const post = req.body.post;
+  const composeData = {
+    title: title,
+    post: post,
+  };
+  posts.push(composeData);
+  res.redirect("/");
+});
+app.get("/posts/:pid", (req, res) => {
+  var pid = _.lowerCase(req.params.pid);
+  posts.forEach((post) => {
+    var postTitle = _.lowerCase(post.title);
+    console.log(pid);
+    if (postTitle === pid) {
+      res.render("post", {
+        postTitle: postTitle,
+        postDescription: post.post,
+      });
+    } else {
+      console.log("not matched");
+    }
+  });
+});
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
